98e0b57a7a9e4de7983c72369b8f3c4b9d1f5bba
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-11 20:58:21 -0500

    Activar modo debug en el servidor para facilitar la depuración

--- servidor.py
+++ servidor.py
@@ -23,4 +23,5 @@ def analizar():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # Cambia a False en producción
+    app.run(host="0.0.0.0", port=port, debug=True)
